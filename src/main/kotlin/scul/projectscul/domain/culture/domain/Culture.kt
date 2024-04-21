524a685425d9979786116186062f2c690314ebf0
tae.y <0211ilyoil@gmail.com>
2024-04-21 14:03:42 +0900

    🦷 :: culture 컬럼 수정

--- src/main/kotlin/scul/projectscul/domain/culture/domain/Culture.kt
+++ src/main/kotlin/scul/projectscul/domain/culture/domain/Culture.kt
@@ -9,6 +9,21 @@ import scul.projectscul.domain.culture.domain.enum.CultureType
 class Culture (
         id: Long,
 
+        @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+        val title: String,
+
+        @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+        val phoneNumber: String,
+
+        @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+        val availableTime: String,
+
+        @Column(nullable = true)
+        val tickerPrice: Int,
+
+        @Column(columnDefinition = "VARCHAR(100)", nullable = false)
+        val cultureLink: String,
+
         @Column(columnDefinition = "DOUBLE", nullable = false)
         val xCoordinate: Double,
 
