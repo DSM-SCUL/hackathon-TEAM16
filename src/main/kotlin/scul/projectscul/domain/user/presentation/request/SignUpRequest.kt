9eba02bb782dc6c3f43534bebd9fba154e810356
tae.y <0211ilyoil@gmail.com>
2024-04-24 10:44:55 +0900

    🦷 :: 회원가입 request 제약조건 추가

--- src/main/kotlin/scul/projectscul/domain/user/presentation/request/SignUpRequest.kt
+++ src/main/kotlin/scul/projectscul/domain/user/presentation/request/SignUpRequest.kt
@@ -5,14 +5,14 @@ import jakarta.validation.constraints.Size
 
 data class SignUpRequest(
         @field:NotBlank
-        @field:Size(max = 10)
+        @field:Size(min = 2, max = 10)
         val name: String,
 
         @field:NotBlank
-        @field:Size(max = 30)
+        @field:Size(min = 6 ,max = 30)
         val accountId: String,
 
         @field:NotBlank
-        @field:Size(max = 30)
+        @field:Size(min = 6 ,max = 30)
         val password: String
 )
