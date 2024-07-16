b3d4e21706cb0bccbc7a56b75d947fe7089c78f0
tae.y <0211ilyoil@gmail.com>
2024-07-16 18:46:01 +0900

    🗂 :: 쓸모없는 엔티티 삭제

--- src/main/kotlin/scul/projectscul/domain/culture/domain/enum/CultureType.kt
+++
@@ -1,7 +0,0 @@
-package scul.projectscul.domain.culture.domain.enum
-
-enum class CultureType {
-    BABY,
-    ELDER,
-    DISABLED
-}
--- src/main/kotlin/scul/projectscul/domain/review/presentation/dto/request/CreateReviewRequest.kt
+++
@@ -1,9 +0,0 @@
-package scul.projectscul.domain.review.presentation.dto.request
-
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Size
-
-data class CreateReviewRequest(
-        @field:NotBlank
-        val content: String
-)
