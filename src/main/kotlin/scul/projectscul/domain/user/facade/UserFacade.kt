b337aaca06f479ab975efc4244b7aa3f7992d2e9
tae.y <0211ilyoil@gmail.com>
2024-07-17 15:58:51 +0900

    ⚙️:: 버그 수정

--- src/main/kotlin/scul/projectscul/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/scul/projectscul/domain/user/facade/UserFacade.kt
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Component
 import java.util.UUID
 
 @Component
-class UserFacade (
+class UserFacade(
         private val userRepository: UserRepository
 ) {
 
     fun getCurrentUser(): User {
-        val id = (SecurityContextHolder.getContext().authentication.principal as UUID)
+        val principal = SecurityContextHolder.getContext().authentication.principal
+
+        // principal이 String 타입이라고 가정하고 UUID로 변환
+        val id = UUID.fromString(principal as String)
+
         return userRepository.findByIdOrNull(id) ?: throw UserNotFoundException
     }
 }
