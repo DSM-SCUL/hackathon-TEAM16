54c1e8b40442ae8cc551aeb011bf627ea065338d
tae.y <0211ilyoil@gmail.com>
2024-07-18 03:04:53 +0900

    🧩:: UUID mypage 에 추가

--- src/main/kotlin/scul/projectscul/domain/user/presentation/response/MyPageResponse.kt
+++ src/main/kotlin/scul/projectscul/domain/user/presentation/response/MyPageResponse.kt
@@ -2,8 +2,10 @@ package scul.projectscul.domain.user.presentation.response
 
 import scul.projectscul.domain.user.domain.Enum.Tier
 import scul.projectscul.domain.user.domain.User
+import java.util.UUID
 
 data class MyPageResponse (
+        val userId: UUID?,
         val score: Int,
         val name: String,
         val tier: Tier,
@@ -12,6 +14,7 @@ data class MyPageResponse (
     companion object {
         fun of(currentUser: User) : MyPageResponse {
             return MyPageResponse(
+                    userId = currentUser.id,
                     score = currentUser.score,
                     name = currentUser.name,
                     tier = currentUser.tier,
