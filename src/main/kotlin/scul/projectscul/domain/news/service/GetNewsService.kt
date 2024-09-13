5092fccbf3c740553fe898a65258aa3fd30eaf18
tae.y <0211ilyoil@gmail.com>
2024-09-13 12:07:07 +0900

    ref :: test

--- src/main/kotlin/scul/projectscul/domain/news/service/GetNewsService.kt
+++ src/main/kotlin/scul/projectscul/domain/news/service/GetNewsService.kt
@@ -2,7 +2,6 @@ package scul.projectscul.domain.news.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import scul.projectscul.domain.news.domain.News
 import scul.projectscul.domain.news.domain.repository.NewsRepository
 import scul.projectscul.domain.news.presentation.response.NewsListResponse
 
